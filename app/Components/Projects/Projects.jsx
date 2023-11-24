e10aace55b461ee7f06778ab20e1adafec2a9f4a
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-11-24 19:30:12 +0300

    projects updated

--- app/Components/Projects/Projects.jsx
+++ app/Components/Projects/Projects.jsx
@@ -16,37 +16,37 @@ const Projects = () => {
         </div>
 
         <Project
-          name={"Manga Verse"}
+          name={"Amdb"}
           description={
             "Manga Verse is a manga viewing app that utilizes a microservice architecture,integrating the Jikan API for manga data retrieval and Puppeteer for chapter scraping. Developed with technologies like Next.js, Express.js, MongoDB, and Puppeteer, Manga Verse offers a seamless user experience. Its key features include user authentication, manga browsing with genre, popularity, and release date filtering options, and an immersive manga reading experience."
           }
-          ImageSrc={`/Assests/manga-verse/manga-verse-home-page.jpg`}
+          ImageSrc={`/Assests/Amdb/amdb.png`}
           id={1}
-          githubLink={"https://github.com/Ree-m/manga-verse"}
-          demoLink={"https://manga-verse-beta.vercel.app/"}
+          githubLink={"https://github.com/switch247/amdb"}
+          demoLink={"https://"}
         />
 
         <Project
         
-          name={"Skinology"}
+          name={"Blogz"}
           description={
-            "Skinology is a full-stack ecommerce app that combines a React frontend with an Express/MongoDB backend. It boasts two interfaces,a user interface and an admin interface. The admin interface gives administrators essential functionalities such as product management through adding, editing, and deleting products while the user interface prioritizes user authentication, along with a fully functional cart for convenient shopping. "
+            "Blogz is a web application designed for bloggers to create and share their content with a wide audience. It provides a user-friendly interface and essential features for managing blog posts. The app is built using modern technologies such as React, Express, MongoDB, and Node.js, ensuring a scalable and efficient platform for bloggers. Blogz includes features like user authentication, post management, search capabilities, and a visually appealing user interface. "
           }
-          ImageSrc={`/Assests/skinology-ecommerce-app-client-onrender-com-1024x768desktop-4d90e6.jpg`}
+          ImageSrc={`/Assests/blogz/Screenshot 2023-08-18 135956.png`}
           id={2}
-          githubLink={"https://github.com/Ree-m/skinology-ecommerce-app"}
-          demoLink={"https://skinology.vercel.app/"}
+          githubLink={"https://github.com/switch247/Blog-App-MERN-frontend/tree/back"}
+          demoLink={"https://bloghub-lcpx.onrender.com/"}
         />
 
         <Project
-          name={"Book Finder"}
+          name={"Bookio"}
           description={
             "Book Finder is a React application that leverages the Google Books API.The key feature of this app is its search functionality, allowing users to search for books by title, author, or publishing date. When a book is selected, detailed information about the book is displayed. Moreover, Book Finder is designed to be fully responsive, ensuring a seamless experience across various devices."
           }
           ImageSrc={`/Assests/book-finder/book-finder-home.jpg`}
           id={3}
-          githubLink={"https://github.com/Ree-m/book-finder"}
-          demoLink={"https://book-finder-tau.vercel.app/"}
+          githubLink={"https://github.com/switch247/Bookio"}
+          demoLink={"https://"}
         />
       </div>
     </div>
